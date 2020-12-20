f4439094a46a2335f7035fc48cb9986e49bd23b6
Giuseppe Persico <giuseppe.persico91@gmail.com>
2020-12-21 00:23:07 +0100

    Removed unused exception

--- src/main/java/com/wafka/exception/CommandExecutionException.java
+++
@@ -1,9 +0,0 @@
-package com.wafka.exception;
-
-public class CommandExecutionException extends Exception {
-	private static final long serialVersionUID = 3104283402115469530L;
-
-	public CommandExecutionException(Exception exception) {
-		super(exception);
-	}
-}
